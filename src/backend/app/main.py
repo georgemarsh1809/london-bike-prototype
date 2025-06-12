985b31d9b98c46b608f9054268c0277bd8dc74b2
George Marsh <georgemarsh1809@gmail.com>
2025-06-12 20:08:32 +0100

    Connected BigQuery to FastAPI - initial query test succesful

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Path
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from pydantic import BaseModel
+from .utils import query_bike_data
 
 app = FastAPI()
 
@@ -21,49 +22,16 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers, adjust as needed
 )
 
-inventories = {
-    
-}
+@app.get("/db/test")
+async def get_bike_data():
+    data = query_bike_data()
+    return data
+
+@app.get("/test")
+def test():
+    return {"test"}
 
 @app.get("/")
 async def root():
     return {"message": "Hello"}
-
-# Playing with Path parameters
-@app.get("/hello/{name}")
-async def hello(name: str):
-    # Data can be take out of the URL using Path parameters, which are defined in the function signature
-    # If the name is not provided, a 'type:missing' error will be raised
-    return {"message": f"Hello, {name}!"}
-
-@app.get("/get-inventory/{inventory_id}")
-# Retrieve an inventory item by its ID - the ID must be an integer greater than 0 and less than 2
-# The description parameter provides additional information about the path parameter for documentation purposes
-async def get_inventory(inventory_id: int = Path(description="The ID of the inventory to retrieve", lt=2, gt=0)):
-    return inventories[inventory_id]
-
-# Playing with Query parameters
-@app.get("/get-by-age")
-async def get_by_age(age: int):
-    # if age is not provided in the URL (i.e. ...?age=24 ), a 'type:missing' error will be raised
-    results = [item for item in inventories.values() if item["age"] == age]
-    if results:
-        return results
-    return {"error": "No items found with the specified age"}
-
-@app.get("/get-by-name")
-async def get_by_name(name: str = None):
-    for item in inventories:
-        if inventories[item].name == name:
-            return inventories[item]
-    return {"error": "Name not found in inventory"}
-
-
-# Playing with Request Body
-@app.post("/create-item{item_id}")
-async def create_item(item_id: int, item: Item):
-    if item_id in inventories:
-        return {"error": "Item with this ID already exists"}
-    else:
-        inventories[item_id] = item
 
---
+++ src/backend/app/utils.py
@@ -0,0 +1,19 @@
+from google.cloud import bigquery
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def query_bike_data():
+    client = bigquery.Client()  # Will auto-use the creds from the env var
+
+    query = """
+    SELECT 
+        count(*)
+    FROM 
+        `bigquery-public-data.london_bicycles.cycle_hire`
+    
+    """
+    query_job = client.query(query)
+    df = query_job.to_dataframe()
+    return df.to_dict(orient="records")
